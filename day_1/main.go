793283eebe324b8628664883e00cbc32f1c3cb15
johopiyo <johnops75@gmail.com>
2024-08-28 16:44:29 +0300

    feat : Day1

---
+++ day_1/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	OpenFile, err := os.Open("./input.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+
+	defer OpenFile.Close()
+
+	scanner := bufio.NewScanner(OpenFile)
+	for scanner.Scan() {
+		characters := scanner.Text()
+		floor := 0
+		for _, ch := range characters {
+			if ch == '(' {
+				floor++
+			} else if ch == ')' {
+				floor--
+			}
+		}
+		fmt.Println(floor)
+	}
+}
